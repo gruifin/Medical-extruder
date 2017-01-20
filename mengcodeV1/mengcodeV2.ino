71a979a4b83b4e8c31ad442a2d05746fb72e8b94
gruifin <ludo.teir@hotmail.com>
2017-01-20 13:24:43 +0100

    new changes, preparation for v3

--- mengcodeV1/mengcodeV2.ino
+++ mengcodeV1/mengcodeV2.ino
@@ -14,7 +14,10 @@
 //                                      -Debug out on the UART <Developer-tool>
 //                                      -Main output to all motors with Interupt?
 //
-//
+//  Code flow chart:  A. Get extrusion data from the UART <Continues> and calculate the extrusion and blend information for the printerhead
+//                    B. Load data for extruders and blender into execute buffers
+//                    C. Execute all buffers simultaneously with an interrupt function <interrupt><?Hz>
+//                            <make sure this interrupt doesn't
 //
 //  ____  ____  ____  ____
 //   ][    ][    ][    ][
@@ -23,12 +26,12 @@
 //  _][_  _][_  _][_  _][_
 //  |  |  |  |  |  |  |  |
 //  |--|  |--|  |--|  |--|
-//  |  |  |  |  |  |  |  |    <4x extruders with lurelock connection
+//  |  |  |  |  |  |  |  |    <4x syringes with lurelock connection
 //  |  |  |  |  |  |  |  |    <6cm filled with 3 ml of gel
 //  |  |  |  |  |  |  |  |
 //  |  |  |  |  |  |  |  |
 //   \/    \/    \/    \/
-//   <________  ________>
+//   [________  ________]
 //           |  |
 //         __|  |__
 //       //        \\
@@ -38,10 +41,18 @@
 //       \\__    __//
 //           |  |       <extruder
 //
+//CODE REDESIGN <v3> <1/20/16>
+//
+//OLD:      The goal of the old code was to execute all the calculations required to extrude the gels depending on a serial data stream.
+//
+//PROBLEM:  1:  the serial data-input-stream uses an interrupt, but the output also uses this interrupt to control all the motors simultaneously
+//          2:  the code performs unnecessary actions that should be done by the 3D printer main board instead, like timming steps and calculating volume/mm for the printing processe
+//
+//          3:
+//NEW_CODE: 1
 
 
-
-//declaring variables
+//declaring variables Pin's
 int enablePin = 4;
 int resetPin = 10;
 
@@ -59,49 +70,32 @@ int stepPin2 = 5; //stepperpin for motor 1
 int stepPin3 = 7; //stepperpin for motor 1
 int stepPin4 = 9; //stepperpin for motor 1
 
-int step [4][2];    //2d array for steps taken by the motors
-int *pstep = step;  //pointer to stepp array
+//system variables
+uint sysvar[6];         //in system variable containing {A%, B%, C%, D%, RPM, dT}
+uint *p = sysvar;       //pointer to stepp array
+uint output[5];         //
+int StepSize = 1;       //stepsize
 
-int volume [5][3];      //volume of gel compartiments and blend chamber
-int *pvolume = volume;  //pointer to vollume array
-
-int StepSize = 32;            //stepsize
-int *pstepzize = StepSize;    //
 
 //initiatin system
 void setup()
 {
   init_steppers();
-  mode(0,1,1,1);
 }
 
 //main loop system
 void loop()
 {
-
-//Ludo rewrite notes: 4
-//waardes defineren buiten de <void loop>
-
-// int vloeistof1;
-// int vloeistof2;
-// int vloeistof3;
-// int vloeistof4;
-// int instel_percentage1;
-// int instel_percentage2;
-// int instel_percentage3;
-// int instel_percentage4;
-//
-// percentage_mengen(vloeistof1,vloeistof2,vloeistof3,vloeistof4);
-// instel_percentage(instel_percentage1,instel_percentage2,instel_percentage3,instel_percentage4,vloeistof1,vloeistof2,vloeistof3,vloeistof4);
-
-//Ludo rewrite notes: 5
-//Er is geen enkele aanwezigheid van een Serial-Debuig
-//Dit is vrij essentieel voor het testen/debuggen van de code
+  input_stream();
+  calc();
 
 }
 
 
-//new function stepper-controlers
+//Function to control the stepper motors
+//Rewrite notes:
+//    -make it so that he 4 motors can output at once   <<this will require a splitted function where on the first half runs in the main loop and the other half in the interrupt loop
+//    -make a step error detection? or juist reduce the accuarecy of the steps
 void stepper(int Motor, int Direction, int Steps, int StepSize) //void stepper(motor: 1-4, Direction: 0-1, Steps: infinite, StepSize: 2,4,8,16,32)
 {
     //local variables
@@ -151,10 +145,65 @@ void stepper(int Motor, int Direction, int Steps, int StepSize) //void stepper(m
 
 }
 
-//PWN control for the blender motor
-blender(int rpm)
+//this function will receive the data over the serial port and load this data into an buffers
+//after this, the function will sort the data in the input buffer and distribute this data over the in-system-buffers
+void input_stream()
 {
-  //write PWN controller
+  if(Serial.avaliable())
+  {
+    SerialIn = Serial.Read();
+  }
+  SerialInArray = toCharArray(SerialIn);
+
+}
+
+
+//this function will perform all the calculations neded between the Serial-input and the final motor-control-actions
+void calc(char array_in_raw[80], char array_steppers[4][80] ,char array_system_settings[80])
+{
+  uint array_in[5]; //the separated values of the AURT-input, internal variable
+
+  int a;    //counter 1 //input array position
+  int b;    //counter 2 //char-lenght of integer values
+  int c;    //counter 3 //counter for construction integers <<<eliminated <<<replaced by B!!
+  int d;    //counter 4 //counter of processed sigits of the imput array, used to store each vallue at the desired location
+
+  //split all the data
+  for(a=0; a<sizeof(array_in); a++)
+  {
+    while(array_in_raw[a]=!0)
+    {
+      b++;
+    }else{
+      for(b; b>0; c--)
+      {
+        array_in[d] = array_in_raw[a-b]*pow(10, b-1); <still working on the corret input data splitting><tonight>
+      }
+      d++; //counter to indicate vallues of inpunt, will increase after each blank space between 2 values.
+    }
+  }
+
+  //assign all the variables to the system buffers, peforms a shift and add on all the buffers
+
+}
+
+//BufferShift
+//Used in: Calc
+//function for shifting an filling the buffers
+//note1: need to use pointers as c++ can't return arrays
+
+void BufferShift(uint input_vall, uint *fill_arr,, uint array_size)
+{
+  for(uint i=array_size; i>0; i--)
+  {
+    *()=*();
+  }
+}
+
+//Outpust side, this function controls al the motors simultaneously
+void output()
+{
+
 }
 
 //this function manages the fluid composition that will exit the printhead on time interval <time - delay>
@@ -173,6 +222,8 @@ void blend(int8_t v1, int8_t v2, int8_t v3, int8_t v4, int8_t flow, int8_t speed
 }
 
 
+///need to combine these 2 functions
+///need to clean the code, and make them compatible with
 int percentage_mengen(int vloeistof1,int vloeistof2,int vloeistof3,int vloeistof4)
 {
   int steps_total;
@@ -255,17 +306,10 @@ void instel_percentage(int gewenst_percentage_1, int gewenst_percentage_2, int g
    }
 
 }
-
-
-void mode(int enable, int mode0, int mode1, int mode2)
-{
-  digitalWrite(enablePin, enable);
-  digitalWrite(mode0Pin, mode0);
-  digitalWrite(mode1Pin, mode1);
-  digitalWrite(mode2Pin, mode2);
-}
+///
 
 void init_steppers(){
+  mode(0,1,1,1);
   pinMode(enablePin, OUTPUT);
   pinMode(resetPin, OUTPUT);
   pinMode(mode0Pin, OUTPUT);
